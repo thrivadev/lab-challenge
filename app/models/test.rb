e760bdcc22092d74f639a0ed711867ae998b3b12
tom livesey <tjlivesey@gmail.com>
2018-04-17 16:15:04 +0100

    Remove price from test data

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,43 +4,37 @@ class Test
       id: 'CHO',
       name: 'Cholesterol',
       sample_volume_requirement: 100,
-      sample_tube_type: :yellow,
-      price: 1400
+      sample_tube_type: :yellow
     },
     {
       id: 'LFT',
       name: 'Liver function',
       sample_volume_requirement: 60,
-      sample_tube_type: :yellow,
-      price: 800
+      sample_tube_type: :yellow
     },
     {
       id: 'VITD',
       name: 'Vitamin D',
       sample_volume_requirement: 120,
-      sample_tube_type: :yellow,
-      price: 900
+      sample_tube_type: :yellow
     },
     {
       id: 'B12',
       name: 'Vitamin B12',
       sample_volume_requirement: 180,
-      sample_tube_type: :yellow,
-      price: 1700
+      sample_tube_type: :yellow
     },
     {
       id: 'HBA1C',
       name: 'HbA1C',
       sample_volume_requirement: 40,
-      sample_tube_type: :purple,
-      price: 1700
+      sample_tube_type: :purple
     },
     {
       id: 'FBC',
       name: 'Full blood count',
       sample_volume_requirement: 80,
-      sample_tube_type: :purple,
-      price: 400
+      sample_tube_type: :purple
     }
   ]
 end
